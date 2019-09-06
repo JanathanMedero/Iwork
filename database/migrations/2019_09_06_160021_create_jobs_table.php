1bc6272b6fbcc0a4df744fcc0e01298d0386e1b0
Janathan <medero.janathan@gmail.com>
2019-09-06 11:08:54 -0500

    Building database

--- database/migrations/2019_09_06_160021_create_jobs_table.php
+++ database/migrations/2019_09_06_160021_create_jobs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateJobsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('jobs', function (Blueprint $table) {
             $table->Increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedInteger('employee_id');
+
+            $table->foreign('employee_id')->references('id')->on('employees');
+            $table->string('job');
+            $table->decimal('salary', 8, 2);
+            $table->string('time_in_the_company');
+
             $table->timestamps();
         });
     }
@@ -31,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('jobs');
     }
 }
